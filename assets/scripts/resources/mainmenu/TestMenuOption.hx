df658723d0de38d9c02d6d2f39105721adaa4da0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-24 20:23:59 -0500

    Test Menu Option now has a description text

--- assets/scripts/resources/mainmenu/TestMenuOption.hx
+++ assets/scripts/resources/mainmenu/TestMenuOption.hx
@@ -1,9 +1,36 @@
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
 import states.mainmenu.MainMenuOption;
 
 class TestMenuOption extends MainMenuOption
 {
+	public var description:FlxText;
+	public var descriptionTween:FlxTween;
+
 	override public function new()
 	{
 		super('test');
+
+		description = new FlxText(0, 0, FlxG.width, "This is a test Menu Option", 16);
+		description.scrollFactor.set();
+		description.color = FlxColor.WHITE;
+		description.alignment = 'right';
+		FlxG.state.add(description);
+
+		onSelect.add(() ->
+		{
+			FlxTween.cancelTweensOf(description);
+			FlxTween.tween(description, {alpha: 1}, .25);
+		});
+
+		onUnselect.add(() ->
+		{
+			FlxTween.cancelTweensOf(description);
+			FlxTween.tween(description, {alpha: 0}, .25);
+		});
+
+		description.alpha = 0;
 	}
 }
